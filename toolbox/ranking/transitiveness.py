6dbe48550f70dc5a0c0a34eba09214a3bd904d97
leonardolepus <leonardolepus@gmail.com>
2014-05-21 19:30:07 +0800

    changed transitiveness calculationg to use beta distribution to estimate p

--- toolbox/ranking/transitiveness.py
+++ toolbox/ranking/transitiveness.py
@@ -1,3 +1,6 @@
+from scipy.stats import beta
+
+
 '''
 in comments of this module,
 a-b means there's an edge between a and b
@@ -7,21 +10,34 @@ a-->b means there's a path between a and b
 '''
 
 
-def trans(g, e):
+def binom_interval(success, total, confint = 0.95):
+    quantile = 1 - confint
+    lower = beta.ppf(quantile, success, total - success + 1)
+    #upper = beta.ppf(1 - quantile, success + 1, total - success)
+    return lower
+    
+def trans(g, e, method = 'beta'):
     #calculates transitiveness of an edge in a graph
     #at_as = P(a-t|a-s)
     #as_at = P(a-s|a-t))
+    if method == 'ratio':
+        func = lambda a, b: b and 1.0 * a / b
+    elif method == 'beta':
+        func = binom_interval
+    else:
+        func = method
+
     s, t = e
     s_neighbors = g.neighbors(s)
     t_neighbors = g.neighbors(t)
     s_neighbors.remove(t)
     t_neighbors.remove(s)
     common_neighbors = [n for n in s_neighbors if n in t_neighbors]
-    return {'at_as' : 1.0 * len(s_neighbors) and 1.0 * len(common_neighbors) / len(s_neighbors),
-            'as_at' : 1.0 * len(t_neighbors) and 1.0 * len(common_neighbors) / len(t_neighbors)}
+    return {'at_as' : func(len(common_neighbors), len(s_neighbors)),
+            'as_at' : func(len(common_neighbors), len(t_neighbors))}
 
 
-def ditrans(g, e, conf = None):
+def ditrans(g, e, method = 'beta'):
     #calculates transitiveness of an edge in a digraph
     #four types of transitiveness are defined, they are:
     #at_as = P(a->t|a->s)
@@ -30,6 +46,13 @@ def ditrans(g, e, conf = None):
     #ta_sa = P(t->a|s->a)
     #bt_bs = P(b-t|b-s)
     #bs_bt = P(b-s|b-t)
+    if method == 'ratio':
+        func = lambda a, b: b and 1.0 * a / b
+    elif method == 'beta':
+        func = binom_interval
+    else:
+        func = method
+
     s, t = e
     s_succ = set(g.successors(s))
     s_pred = set(g.predecessors(s))
@@ -40,10 +63,10 @@ def ditrans(g, e, conf = None):
     c_succ = s_succ & t_succ
     c_pred = s_pred & t_pred
     c_neig = s_neig & t_neig
-    return {'at_as' : 1.0 * len(s_pred) and 1.0 * len(c_pred) / len(s_pred),
-            'as_at' : 1.0 * len(t_pred) and 1.0 * len(c_pred) / len(t_pred),
-            'sa_ta' : 1.0 * len(c_succ) and 1.0 * len(c_succ) / len(t_succ),
-            'ta_sa' : 1.0 * len(s_succ) and 1.0 * len(c_succ) / len(s_succ),
-            'bt_bs' : 1.0 * len(s_neig) and 1.0 * len(c_neig) / len(s_neig),
-            'bs_bt' : 1.0 * len(t_neig) and 1.0 * len(c_neig) / len(t_neig)}
+    return {'at_as' : func(len(c_pred), len(s_pred)),
+            'as_at' : func(len(c_pred), len(t_pred)),
+            'sa_ta' : func(len(c_succ), len(t_succ)),
+            'ta_sa' : func(len(c_succ), len(s_succ)),
+            'bt_bs' : func(len(c_neig), len(s_neig)),
+            'bs_bt' : func(len(c_neig), len(t_neig))}
 
