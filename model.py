b95ae5d71337d36f0371977c9afe14e52bf6bf96
leonardolepus <leonardolepus@gmail.com>
2014-05-30 10:55:43 +0800

    hybrid model continue

--- model.py
+++ model.py
@@ -1,7 +1,8 @@
 import pickle
 from collections import Counter
-from math import log
+from math import log, exp
 
+import numpy as np
 from scipy import stats
 from matplotlib import pyplot as plt
 import networkx as nx
@@ -41,26 +42,53 @@ lin_regr(dgr, log_occr)
 
 lin_regr(log_dgr, log_occr)
 
-#log(1 - F(d)) = c - x * log(d + amx)
+#log(1 - F(d)) = x*log(m+amx) - x * log(d + amx)
 #where x = 2 / (1 - a)
-F = lambda d: 1.0*len([dgr for dgr in degrees if dgr < d])/len(degrees)
+F = lambda d: 1.0*len([dgr for dgr in degrees if dgr <= d])/len(degrees)
 
 m = 1.0*sum(degrees)/len(degrees)/2
-d_range = range(1, max(degrees)+1)
+d_range = range(1, max(d_range))
 
-a = 0.1
-a_prime = 0.5
-while abs(a_prime - a) > 0.001:
-    a = a_prime
+m = 10
+a = 0.7
+m_prime = 40
+a_prime = 0.9
+while abs(m_prime-m) > 2 or abs(a_prime-a) > 0.1:
+    a = (a+a_prime)/2
+    m = (m+m_prime)/2
+    x = 2.0/(1-a)
+    right = [log(d+a*m*x) for d in d_range]
+    left = [log(1-F(d)) for d in d_range]
+    slope, intercept, r, p, std_err = stats.linregress(right, left)
+    print slope, intercept, r**2, p, std_err
+    lin_regr(right, left)
+    a_prime = 1+2.0/slope
+    x_prime = 2.0/(1-a_prime)
+    m_prime = exp(intercept/x_prime)/(1+a_prime*x_prime)
+    print a_prime, m_prime
+
+m = 40
+for a in np.arange(0.01, 0.97, 0.05):
     x = 2.0/(1-a)
     right = [log(d+a*m*x) for d in d_range]
     left = [log(1-F(d)) for d in d_range]
     slope, intercept, r, p, std_err = stats.linregress(right, left)
     lin_regr(right, left)
-    #slope is x
-    a_prime = 1-2.0/slope
+    #slope is -x
+    a_prime = 1+2.0/slope
     print a
     print a_prime
     print abs(a_prime - a)
+    if abs(a_prime - a) < 0.01:
+        break
 
-print a
+m = 40
+a = 0.06
+x = 2.0/(1-a)
+F_prime = lambda d: 1-((m+a*m*x)/(d+a*m*x))**x
+f = [F(d) for d in d_range]
+f_prime = [F_prime(d) for d in d_range]
+plt.hold(True)
+plt.scatter(d_range, f)
+plt.scatter(d_range, f_prime)
+plt.show()
