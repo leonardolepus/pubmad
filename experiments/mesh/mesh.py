dec72b1a372e88be927f2010b7c816835ec82d46
leonardolepus <leonardolepus@gmail.com>
2014-07-31 18:05:57 +0800

    retrieve graphs

--- experiments/mesh/mesh.py
+++ experiments/mesh/mesh.py
@@ -1,24 +1,70 @@
 #used to retrieve mesh terms from pubmed and construct a network
+import itertools
 
-from Bio import Entrez, Medline
+import matplotlib.pyplot as plt
+import networkx as nx
+from Bio import Entrez
+
 
 EMAIL = 'leonardolepus@gmail.com'
-keywords = 'hydrogen sulfide[Mesh]'
 
-Entrez.email = EMAIL
-handle = Entrez.esearch(db = 'pubmed', term = keywords, retmax = 10000000, retmode = 'text')
-records = Entrez.read(handle)
-handle.close()
-#for id in records['IdList']:
-#id = records['IdList'][0]
-id = '24912238'
-handle = Entrez.efetch(db = 'pubmed', id = id, retmode = 'xml')
-entre = Entrez.read(handle)
-try:
-    mesh = [m['DescriptorName'] for m in entre[0]['MedlineCitation']['MeshHeadingList']]
-    add_to_graph(mesh, g)
-except KeyError:
-    pass
 
-def add_to_graph(entity_list, g):
-    
+def retrieve_graph_from_query(query, graph, max_records = null):
+    Entrez.email = EMAIL
+    #get number of records
+    handle = Entrez.esearch(db = 'pubmed', term = query, retmode = 'text')
+    records = Entrez.read(handle)
+    handle.close()
+    records_number = records['Count']
+    #get uids
+    handle = Entrez.esearch(db = 'pubmed', term = query, retmax = records_number, retmode = 'text')
+    records = Entrez.read(handle)
+    handle.close()
+    uids = records['IdList']
+    if max_records > 0:
+        uids = uids[0:max_records] if max_records < len(uids) else uids
+    for uid in uids:
+        handle = Entrez.efetch(db = 'pubmed', id = uid, retmode = 'xml')
+        entre = Entrez.read(handle)
+        handle.close()
+        try:
+            mesh = [str(m['DescriptorName']) for m in entre[0]['MedlineCitation']['MeshHeadingList']]
+            edge_list = [e for e in itertools.combinations(mesh, 2)]
+            graph.add_edges_from(edge_list)
+            print 'retrieved %d of %d' % (uids.index(uid) + 1, len(uids))
+        except KeyError:
+            pass
+
+
+g = nx.MultiGraph()
+query = 'hydrogen sulfide[Mesh]'
+retrieve_graph_from_query(query, g)
+print 'graph has %d nodes, %d edges' % (g.number_of_nodes(), g.number_of_edges())
+nx.draw(g)
+plt.show()
+
+
+def retrieve_bin_occr(entity1, entity2, field1 = ' ', field2 = ' '):
+    Entrez.email = EMAIL
+    query = '''"%s"[%s] AND "%s"[%s]''' % (entity1, field1, entity2, field2)
+    print query
+    handle = Entrez.esearch(db = 'pubmed', term = query, retmode = 'text')
+    records = Entrez.read(handle)
+    handle.close()
+    records_number = int(records['Count'])
+    print records_number
+    return records_number
+
+
+def retrieve_graph_from_entities(entities, graph):
+    for e1, e2 in itertools.combinations(entities, 2):
+        occr = retrieve_bin_occr(e1, e2)
+        edge_list = [(e1, e2) for i in range(occr)]
+        graph.add_edges_from(edge_list)
+
+
+entities = ['hydrogen sulfide', "alzheimer's", "parkinson's", 'stroke', 'inflammation', 'apoptosis']
+g = nx.MultiGraph()
+retrieve_graph_from_entities(entities, g)
+nx.draw(g, with_lables = True)
+plt.show()
