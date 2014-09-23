9be46e3461707882c38e4d1fceaf2a8c0c0c4d0b
leonardolepus <leonardolepus@gmail.com>
2014-09-23 21:36:56 +0800

    populate mongodb with edges

---
+++ toolbox/graph_io/mongodb/populate.py
@@ -0,0 +1,47 @@
+import sys, time
+
+import pymongo
+from pymongo import MongoClient
+from pymongo.errors import OperationFailure
+
+ADDR = 'localhost'
+PORT = 27017
+term_tag = {}
+
+client = MongoClient(ADDR, PORT)
+db = client['pubmad']
+coll = db['graph']
+posts = []
+for line in sys.stdin:
+    line = line.stripe()
+    edge, ref = line.split('\t')
+    source, target = edge.split('==')
+    ref = ref.split(';;')
+    post = {'source' : source,
+            'source_tags' : term_tag.get(source, []),
+            'target' : target,
+            'target_tags' : term_tag.get(target, []),
+            'edge_tags' : [],
+            'refs' : ref,
+            'weight' : len(ref)}
+    posts.append(post)
+    if len(posts) > 100:
+        try:
+            coll.insert(posts)    #if error, pickle dump posts, upsert later
+        except OperationFailure:
+            with open('failed_posts'+str(time.time()), 'w') as f:
+                pickle.dump(posts, f)
+            print 'insert error, failed posts saved'
+        finally:
+            posts = []
+if posts:
+        try:
+            coll.insert(posts)    #if error, pickle dump posts, upsert later
+        except OperationFailure:
+            with open('failed_posts'+str(time.time()), 'w') as f:
+                pickle.dump(posts, f)
+            print 'insert error, failed posts saved'
+        finally:
+            posts = []
+
+
